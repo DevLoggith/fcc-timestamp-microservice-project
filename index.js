3adbe91c70d9395c6088de2021dbdbcc7995f16f
logan <92327020+DevLoggith@users.noreply.github.com>
2025-09-15 13:18:15 -0400

    create initial date endpoint
    update html with personal references

--- index.js
+++ index.js
@@ -25,6 +25,10 @@ app.get("/api/hello", function (req, res) {
 });
 
 
+app.get("/api/:date", (req, res) => {
+  
+});
+
 
 // Listen on port set in environment variable or default to 3000
 var listener = app.listen(process.env.PORT || 3000, function () {
